056bd23e18864ecbac39625c7dd603ab5a670f5b
Kesri N. Shukla <138159327+kesri9211@users.noreply.github.com>
2023-07-07 00:53:12 +0530

    create frequency array to find frequency of element.java

--- Introduction/4-frequency.java
+++ Introduction/4-frequency.java
@@ -1,19 +1,23 @@
 import java.util.*;
 public class Main
-{   
+{
+    static int[] makefreqarray(int ar[]){
+       
+         int freq[]=new int[100002];
+        for(int i=0;i<ar.length;i++)
+            freq[ar[i]]++;
+        return freq;
+    }
 	public static void main(String[] args) {
-	  Scanner sc=new Scanner(System.in);
+	    Scanner sc=new Scanner(System.in);
 		System.out.println("enter size of array");
 		int n=sc.nextInt();
 		int ar[]=new int[n];
-    
 		System.out.println("enter array element");
 		for(int i=0;i<n;i++)
 		ar[i]=sc.nextInt();
 		
-		 int freq[]=new int[100002];         //making frequency array
-        for(int i=0;i<ar.length;i++)
-            freq[ar[i]]++;
+		int freq[]=makefreqarray(ar);
 		
 		System.out.println("number of queries?");
 		int q=sc.nextInt();
