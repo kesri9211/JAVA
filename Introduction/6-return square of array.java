cf4da3a41851b1681b46548944fafea7d8a696a0
Kesri N. Shukla <138159327+kesri9211@users.noreply.github.com>
2023-07-08 19:03:31 +0530

    return square of non decreasing array in non decreasing form.java

---
+++ Introduction/6-return square of array.java
@@ -0,0 +1,52 @@
+import java.util.*;
+public class Main
+{
+    static int[] reverse(int ans[]){
+        int j=ans.length-1;
+        int i=0;
+        while(i<=j){
+            int temp=ans[i];
+            ans[j]=ans[i];
+            ans[i]=temp;
+            i++; 
+            j--;
+        }
+        return ans;
+    }
+    
+    static void sortsquare(int ar[]){
+        int l=0,i=0;
+        int n=ar.length;
+        int r=n-1;
+        int ans[]=new int[n];
+        while(l<=r){
+            if(Math.abs(ar[l])>Math.abs(ar[r])){
+                ans[i]=ar[l]*ar[l];
+                l++;
+                i++;
+            }
+            else if(Math.abs(ar[l])<Math.abs(ar[r])){
+                ans[i]=ar[r]*ar[r];
+                r--;
+                i++;
+            }
+        }
+        reverse(ans);
+        System.out.println("shorted array");
+         for(int k=0;k<n;k++)
+		        System.out.print(ans[k]+" ");
+    }
+    
+    
+	public static void main(String[] args) {
+	    Scanner sc=new Scanner(System.in);
+		System.out.println("enter size of array");
+		int n=sc.nextInt();
+		int ar[]=new int[n];
+		System.out.println("enter element of array");
+		for(int i=0;i<n;i++)
+		ar[i]=sc.nextInt();
+		
+		sortsquare(ar);
+		    }
+}
