00775d59e43709f611e58935209b824d3cc1332c
Kesri N. Shukla <138159327+kesri9211@users.noreply.github.com>
2023-08-26 15:30:57 +0530

    problem 2 on string: sort string lexicographically using selection sort

---
+++ DS in java/2-Shorting/sort string.java
@@ -0,0 +1,36 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+import java.util.*;
+public class Main
+{      //selection sort
+    static void sort(String ar[]){
+        int n=ar.length;
+        for(int i=0;i<n-1;i++){
+            int min=i;    //store value of smallest index
+            for(int j=i+1;j<n;j++){
+                if(ar[j].compareTo(ar[min])<0) min=j;
+                
+                /* j<min =-1 
+                   j>min =1 
+                   j=min =0.  */
+            }
+            //swap ar[min] and ar[i]
+            String temp=ar[min];
+            ar[min]=ar[i];
+            ar[i]=temp;
+        }
+    }
+	public static void main(String[] args) {
+	    String ar[]={"kesri","arpita","ayush","rohit","puneet","girish"};
+		System.out.println("ans");
+		sort(ar);
+		for(String val:ar)
+		System.out.print(val+", ");
+	}
+}
