54020e8269619c3b6177f0a8d9268c61e58030d0
Kesri N. Shukla <138159327+kesri9211@users.noreply.github.com>
2023-10-14 15:19:33 +0530

    Count sort
    TC=O(N+K)
    SC=N+K

--- DS in java/2-Shorting/Count sort.java
+++ DS in java/2-Shorting/Count sort.java
@@ -10,20 +10,19 @@ public class Main
         }
         return max;
     }
-    //create freq array
-    static int [] CreatefreqArr(int ar[]){
+    static void display(int ar[]){
+        for(int i:ar)
+		System.out.print(i+" ");
+    }
+    static void basicCountSort(int ar[]){
         int max=maxValue(ar); 
+        //1-create freq array
         int freq[]=new int[max+1];
         for (int i=0;i<ar.length;i++){
             freq[ar[i]]++;
         }
-        return freq;
-    }
-	public static void main(String[] args) {
-		int ar[]={4,3,1,5,3,1,3,5};
-		int freq[]=CreatefreqArr(ar);
-		int k=0;
-		//store element into answer array using frequency(occurance) to get sorted array
+        int k=0;
+		//2-store element into answer array using frequency(occurance) to get sorted array
 		for(int i=0;i<freq.length;i++){
 		    int c=freq[i];
 		 //when freq is greater than zero enter number equals to 'frequency' number of time
@@ -32,8 +31,34 @@ public class Main
 		        c--;
 		    }
 		}
-		//print
-		for(int i:ar)
-		System.out.print(i+" ");
+    }
+                  /*-----------STABLE--------*/
+    static void CountSort(int ar[]){
+        int n=ar.length;
+        int max=maxValue(ar); 
+        int output[]=new int[n];
+        //1-create freq array
+        int freq[]=new int[max+1];
+        for (int i=0;i<ar.length;i++){
+            freq[ar[i]]++;
+        }
+        //2-create prefix sum array of freq array
+        for(int i=1;i<freq.length;i++)
+        freq[i]=freq[i]+freq[i-1]; //update there only
+        
+        //3-find index of each element in original array and put in output array
+        //traverse backward
+        for(int i=n-1;i>=0;i--){
+            int index=--freq[ar[i]];
+    //Decrement value of prefixSumArray,which gives new position of index(element in original array)
+            output[index]=ar[i];
+        }
+        display(output);
+    }
+    
+	public static void main(String[] args) {
+		int ar[]={4,3,1,5,3,1,3,5};
+		//basicCountSort(ar);
+		CountSort(ar);
 	}
 }
