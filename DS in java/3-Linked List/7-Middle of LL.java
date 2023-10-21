57cd368a4c5cbf61fe91e769cd6144a13ce3ef25
Kesri N. Shukla <138159327+kesri9211@users.noreply.github.com>
2023-10-21 11:38:17 +0530

    Find Middle of Linked list

---
+++ DS in java/3-Linked List/7-Middle of LL.java
@@ -0,0 +1,17 @@
+/*Given the head of a singly linked list, return the middle node of the linked list.
+If there are two middle nodes, return the second middle node.*/
+
+class Solution {
+    public ListNode middleNode(ListNode head) {
+      //  if(head.next==null) return head;
+
+        ListNode slow=head;
+        ListNode fast=head;
+
+        while(fast!=null && fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+        }
+        return slow;
+    }
+}
