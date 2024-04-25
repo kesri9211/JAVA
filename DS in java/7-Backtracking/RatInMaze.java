c17f66362f26bd6793268ad3099e7d283c010e57
Kesri N. Shukla <138159327+kesri9211@users.noreply.github.com>
2024-04-25 10:32:43 +0530

    Update RatInMaze- 2 direction
    m1- recursion
    m2-dp

--- DS in java/7-Backtracking/RatInMaze.java
+++ DS in java/7-Backtracking/RatInMaze.java
@@ -33,3 +33,20 @@ public class RatInMaze1 {
         print(sr+1,sc,er,ec,s+"D");
     }
 }
+
+
+//--------m2-------
+/*You are standing on a point (x, y) and you want to go to origin (0, 0) by taking steps either
+    left or up i.e. from each point you are allowed to move either in (x-1, y) or (x, y-1). Find the number of paths from point to origin.*/
+   int dp[][]=new int[n+1][m+1];
+        for(int i=0;i<=n;i++){
+            for(int j=0;j<=m;j++){
+                if(i==0 || j==0){
+                    dp[i][j]=1;
+                }
+                else{
+                    dp[i][j]=(dp[i-1][j]+dp[i][j-1])%1000000007;
+                }
+            }
+        }
+        return dp[n][m];
