71d5c9b52557442ea30945ebff6935bbdd19d9a4
Kesri N. Shukla <138159327+kesri9211@users.noreply.github.com>
2024-09-02 16:32:40 +0530

    2. Add Two Numbers

---
+++ DS in java/3-Linked List/9-addTwoLL.java
@@ -0,0 +1,39 @@
+/**
+Input: l1 = [2,4,3], l2 = [5,6,4]
+Output: [7,0,8]
+Explanation: 342 + 465 = 807.
+
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode dummy=new ListNode(0);
+        ListNode temp=dummy;
+        int carry=0;
+        //move (add) till 
+        while(l1!=null || l2!=null || carry==1){
+            int sum=0;
+            if(l1!=null){
+                sum+=l1.val;
+                l1=l1.next;
+            }
+            if(l2!=null){
+                sum+=l2.val;
+                l2=l2.next;
+            }
+            sum+=carry; //add carry also
+            carry=sum/10;
+            temp.next=new ListNode(sum%10); //add new node with value sum%10
+            temp=temp.next;
+        }
+
+        return dummy.next;
+    }
+}
